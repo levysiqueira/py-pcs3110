1a8221541631b64348bf3231cdf145ef99b6a387
Fabio Levy Siqueira <levy.siqueira@gmail.com>
2019-08-13 19:07:57 -0300

    Padrão dos comentários corrigido

--- modulo1/pilha.py
+++ modulo1/pilha.py
@@ -3,7 +3,8 @@ Algoritmos de PCS3110 em Python
 Módulo 1 - Estruturas de dados
 """
 class Pilha:
-    '''Uma pilha implementada com uma lista, com as operações push e pop.'''
+    """Uma pilha implementada com uma lista, com as operações push e pop.
+    """
     def __init__(self):
         self.topo = -1
         self.elementos = []
@@ -12,9 +13,10 @@ class Pilha:
         return self.topo + 1
 
     def pop(self):
-        '''Retira o elemento no topo da pilha e o retorna.
+        """Retira o elemento no topo da pilha e o retorna.
 
-        Caso a pilha esteja vazia, joga um ValueError'''
+        Caso a pilha esteja vazia, joga um ValueError
+        """
         if self.topo == -1:
             raise ValueError('Underflow')
         #return self.elementos.pop(self.topo)
@@ -24,11 +26,13 @@ class Pilha:
         return retorno
 
     def push(self, valor):
-        '''Adiciona o valor ao topo da pilha.'''
+        """Adiciona o valor ao topo da pilha.
+        """
         self.topo += 1
         self.elementos.append(valor)
 
     def is_empty(self):
-        '''Retorna True caso a pilha esteja vazia ou False
-        caso contrário.'''
+        """Retorna True caso a pilha esteja vazia ou False
+        caso contrário.
+        """
         return len(self.elementos) == 0
